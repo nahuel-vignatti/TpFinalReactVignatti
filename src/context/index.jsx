31f185cacaf6ead9fa493fa6f8364dea599ce8fb
nahuel <nahuel1024@gmail.com>
2023-03-28 14:25:49 -0300

    avance de context

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,8 +1,26 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+
 export const Context = createContext();
 
 function CustomProvider({children}){
-    return <Context.Provider value="Hola mundo">{children}</Context.Provider>
+
+    const [itemsAddedQuantity, setItemsAddedQuantity] = useState(0);
+
+    const onAdd = () => {
+        setItemsAddedQuantity((oldState) => oldState +1);
+    }
+
+    const onRemove = () => {
+        setItemsAddedQuantity((oldState) => oldState -1);
+    }
+
+    const value = {
+        itemsAddedQuantity,
+        onAdd,
+        onRemove,
+    }
+
+    return <Context.Provider value={value}>{children}</Context.Provider>
 }
 
 export default CustomProvider;
